9e94fbc15abf123f2b52cd339050736fe9ee948e
Jason Corbett <jasoncorbett@gmail.com>
2021-09-02 19:32:25 -0600

    add basic command line parsing library

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	app := &cli.App{
+		Usage: "play sounds when you git push",
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
